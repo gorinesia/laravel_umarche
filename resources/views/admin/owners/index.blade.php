591e02c375bc04ecc6b62982517b24b804df52e2
Akinori Sakakibara <gorion.1213@gmail.com>
2021-09-19 09:03:37 +0900

    image seeder

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -42,7 +42,7 @@
                                     <td class="md:px-4 py-3">
                                         <a href="#" data-id="{{ $owner->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-5s00 rounded">削除</a>
                                     </td>
-                                </form>
+                                  </form>
                                 </tr>
                                 @endforeach
                               </tbody>
